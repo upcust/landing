0f0c6d033d7e1d50a61cd3ad20b1b4eea40af53f
Charles Simon-Meunier <charlessimonmeunier@icloud.com>
2024-05-27 23:12:50 +0200

    Init tailwind & shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="h-screen flex flex-col align-middle justify-center space-y-8">
+        <h1 className="text-6xl font-bold">
+          Libérez votre potentiel de <br />
+          Community Manager
+        </h1>
+        <p className="text-2xl font-medium max-w-[480px] mx-auto">
+          Créez, planifiez, validez, modérez et analysez tous vos posts.
+          <br />
+          Soyez en veille des dernières tendances.
         </p>
+        <Button className="w-fit align-middle mx-auto text-lg font-medium text-black">Je m’inscris</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="text-lg">Première question</AccordionTrigger>
+          <AccordionContent className="text-left text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare luctus nunc, a porta risus dictum
+            vitae. Aenean vel nisl sed sapien convallis varius. Integer tristique convallis arcu, a scelerisque quam
+            porta at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
