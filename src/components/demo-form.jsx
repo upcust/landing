b250fb28921342a4bb55bf4573f4131032841665
Charles Simon-Meunier <charlessimonmeunier@icloud.com>
2024-06-12 23:51:17 +0200

    CTA section with shortcut

--- src/components/demo-form.jsx
+++ src/components/demo-form.jsx
@@ -39,37 +39,64 @@ export default function DemoForm() {
     });
   };
 
+  useEffect(() => {
+    const onDown = (e) => {
+      if (e.key === 'r') {
+        triggerConfettiSideCannons();
+        setCalendlyModalOpen(true);
+      }
+    };
+
+    window.addEventListener('keydown', onDown);
+
+    return () => {
+      window.removeEventListener('keydown', onDown);
+    };
+  }, []);
+
   return (
-    <div className="min-h-[600px] h-screen flex items-center justify-center pb-10" id="demo">
-      <div className="h-3/5 bg-[#2F2C2C] w-full p-10 rounded my-auto text-white flex flex-col justify-between items-start">
-        <div className="max-w-[500px]">
-          <h2 className="font-bold xl:text-4xl md:text-3xl text-2xl mb-1">Demander une démo</h2>
+    <div className="min-h-[900px] h-screen flex items-center justify-center pb-10" id="demo">
+      <div className="h-3/5 bg-[#2F2C2C] w-full p-10 rounded-xl my-auto text-white flex flex-col justify-between items-start relative">
+        <img src="/demo-bg.png" alt="Demo" className="absolute bottom-0 left-0 w-full" />
+        <div className="max-w-[500px] m-auto flex flex-col items-center text-center relative">
+          <span className="bg-primary rounded-full px-5 py-1 text-black absolute -top-10 text-sm font-medium">
+            Démo de 30 minutes gratuite
+          </span>
+          <h2 className="font-bold xl:text-5xl md:text-4xl text-3xl mb-1 mt-10">
+            Laissez nous vous <br />
+            surprendre
+          </h2>
           <p className="text-lg font-light mb-6">
             Découvrez comment Upcust peut vous aider à gérer vos réseaux sociaux en toute simplicité.
           </p>
-          <Input
-            placeholder="Votre nom"
-            className="w-full h-10 mb-4 text-black"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            placeholder="Votre email"
-            className="w-full h-10 mb-4 text-black"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            onBlur={sendLeadInfo}
-          />
+          <div className="flex gap-4">
+            <Input
+              placeholder="Votre nom"
+              className="w-full h-10 mb-4 text-black"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              placeholder="Votre email"
+              className="w-full h-10 mb-4 text-black"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={sendLeadInfo}
+            />
+          </div>
+          <Button
+            onClick={() => {
+              triggerConfettiSideCannons();
+              setCalendlyModalOpen(true);
+            }}
+            className="text-black mt-4"
+          >
+            Je réserve{' '}
+            <kbd className="ml-2 pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+              R
+            </kbd>
+          </Button>
         </div>
-        <Button
-          onClick={() => {
-            triggerConfettiSideCannons();
-            setCalendlyModalOpen(true);
-          }}
-          className="text-black"
-        >
-          Je réserve
-        </Button>
         <PopupModal
           url="https://calendly.com/upcust-app/30min"
           onModalClose={() => setCalendlyModalOpen(false)}
